b77327680650366303c00f60baeef29b026502dd
jan-patrick <jan.schneider@hfg-gmuend.de>
2017-12-21 12:36:48 +0100

    v 1.8.0
    as released in Microsoft Store

--- LostBrick/js/main.js
+++ LostBrick/js/main.js
@@ -937,6 +937,9 @@ function keyInput(e) {
                 case 40: // arrow down
                     playDir = "a";
                     break;
+                case 27: // esc
+                    window.location.reload();
+                    break;
                 default:
                     sideNum = sideNum;
                     buttonPressed = true;
@@ -956,6 +959,9 @@ function keyInput(e) {
                     break;
                 case 40: // arrow down
                     playDir = "s";
+                    break;
+                case 27: // esc
+                    window.location.reload();
                     break;
                 default:
                     sideNum = sideNum;
